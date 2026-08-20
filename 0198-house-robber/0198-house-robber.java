db18a29eb3fa529cd2b8b38599829cc422493363
Jayant Singh <146509801+JayantSingh2005@users.noreply.github.com>
2026-08-20 14:43:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.48%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+       if(nums.length == 1)
+       return nums[0];
+       if(nums.length == 2)
+       return Math.max(nums[0], nums[1]);  
+       int[] track = new int[nums.length];  // to keep a track of max profit
+       track[0] = nums[0];
+       track[1] = Math.max(nums[0], nums[1]);
+       for(int i = 2; i < nums.length; i++)
+       {
+        int take = nums[i] + track[i-2];
+        int skip = track[i-1];
+        track[i] = Math.max(take,skip);
+       } 
+       return track[nums.length-1];
+    }
+}
